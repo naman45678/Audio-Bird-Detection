438d70834da0c3685edaaafcfaec285c0938362a
naman45678 <54641886+naman45678@users.noreply.github.com>
2020-03-04 13:30:29 +0530

    more useful module commited

--- code.py
+++ code.py
@@ -6,3 +6,13 @@ from scipy.fftpack                import fft
 
 from scipy.signal                 import lfilter, hamming
 from scipy.fftpack.realtransforms import dct
+import librosa
+import os
+from sklearn.preprocessing import StandardScaler
+from sklearn import svm
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+import pandas as pd
+from scipy.io import wavfile
